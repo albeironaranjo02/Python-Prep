31ef607bd14005603478e05d0315d69e90559dc9
albeiro <albeiro@example.com>
2023-04-27 13:38:47 -0500

    Solucion Clase 3

--- 03-FlujosDeControl/solucion_de_ejercicios.py
+++ 03-FlujosDeControl/solucion_de_ejercicios.py
@@ -3,7 +3,7 @@
 # 1) Crear una variable que contenga un elemento del conjunto de números enteros y 
 # luego imprimir por pantalla si es mayor o menor a cero
 
-a= 10
+a = 10
 if a > 0:
     print(f"{a} es mayor a 0")
 else: print(f"{a} es menor a cero")
@@ -12,19 +12,70 @@ else: print(f"{a} es menor a cero")
 
 # 2) Crear dos variables y un condicional que informe si son del mismo tipo de dato
 
+a = 10
+b = "10"
+if type(a) == type(b):
+    print(f"Las variables son del mismo tipo: {type(a)}")
+else: print(f"Las variables osn de diferentes valor a es {type(a)} y b es {type(b)}")
 # 3) Para los valores enteros del 1 al 20, imprimir por pantalla si es par o impar
+for i in range (11):
+    if i % 2 == 0:
+        print(f"{i} es par")
+    else: print(f"{i} es impar")
 
 # 4) En un ciclo for mostrar para los valores entre 0 y 5 el resultado de elevarlo a la potencia igual a 3
+for i in range (6):
+    print(f"{i} a la 3 potencia es: {pow(i,3)}")
 
 # 5) Crear una variable que contenga un número entero y realizar un ciclo for la misma cantidad de ciclos
 
-# 6) Utilizar un ciclo while para realizar el factorial de un número guardado en una variable, sólo si la variable contiene un número entero mayor a 0
+a = int(input("Digite un numero: "))
+for i in range (a):
+    print(f"Iteracion {i+1} de {a}")
+
+# 6) Utilizar un ciclo while para realizar el factorial de un número guardado en
+#  una variable, sólo si la variable contiene un número entero mayor a 0
+
+num = int(input("Ingrese un numero para sacar el factorial:"))
+factorial = 1
+valor = 1
+if num > 0:
+    while valor <= num:
+        factorial *= valor
+        valor +=1
+    print("El factorial de", num, "es", factorial)
+
+else: print("Solo es posible para mayores a 1")
 
 # 7) Crear un ciclo for dentro de un ciclo while
+a = 8
+iterador = 0
+while iterador <= a:
+    for i in range (a):
+        if i == iterador:
+            print (f"El iterador del while es igual al iterador del for")
+    iterador += 1
+
+
 
 # 8) Crear un ciclo while dentro de un ciclo for
 
+for i in range (3):
+    a=0
+    while a < 3:
+        print (f"Iterador del while menor al ietardor del rango 3")
+        a += 1
+
 # 9) Imprimir los números primos existentes entre 0 y 30
+
+for i in range(1,31):
+    cont = 0
+    for j in range (1,i + 1):
+        if i % j == 0:
+            cont += 1
+    if cont == 2:
+        print(f"{i} es un numero primo")
+
 
 # 10) ¿Se puede mejorar el proceso del punto 9? Utilizar las sentencias break y/ó continue para tal fin
 
